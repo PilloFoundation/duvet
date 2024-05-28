aee5e64b76ba100a0ae1e64c4c129bb9c76a4669
PKWadsworth <pkwads2@gmail.com>
2024-05-29 10:07:52 +1200

    Added more useful exports

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export { Kint } from "./core/Kint";
+export { createKint } from "./core/createKint";
+
+export { Middleware } from "./core/models/middleware/Middleware";
+export { PostProcessingMiddleware } from "./core/models/middleware/PostProcessingMiddleware";
+export { PreprocessingMiddleware } from "./core/models/middleware/PreprocessingMiddleware";
+
+export { KintRequest } from "./core/models/KintRequest";
+export { KintResponse } from "./core/models/KintResponse";
