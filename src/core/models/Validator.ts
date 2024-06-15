62b36a382541f7aede680a12c7b33e2d5ac3cfa9
PKWadsworth <pkwads2@gmail.com>
2024-06-15 14:42:58 +1200

    Fixed formatting error

--- src/core/models/Validator.ts
+++ src/core/models/Validator.ts
@@ -26,37 +26,8 @@ export type ValidatedData<Validators extends ValidatorArray> = {
   [Validator in Validators[number] as ExtractField<Validator>]: ExtractData<Validator>;
 };
 
-export type ExtractField<V> = V extends Validator<infer Field, unknown>
-  ? Field
-  : never;
+export type ExtractField<V> =
+  V extends Validator<infer Field, unknown> ? Field : never;
 
-export type ExtractData<V> = V extends Validator<string, infer Data>
-  ? Data
-  : never;
-
-// const makeV: <Field extends string, Data>(
-//   field: Field,
-//   data: Data
-// ) => Validator<Field, Data> = (field, data) => {
-//   return {
-//     validate: () => {
-//       return {
-//         isValid: true,
-//         field,
-//         parsedData: data,
-//       };
-//     },
-//   };
-// };
-
-// const test = [
-//   makeV("test", 5),
-//   makeV("another", { some: "data" }),
-//   makeV("third", "string"),
-// ];
-
-// type Test = ValidatedData<typeof test>;
-
-// const x: Test = {
-//   another: { some: "data" },
-// };
+export type ExtractData<V> =
+  V extends Validator<string, infer Data> ? Data : never;
