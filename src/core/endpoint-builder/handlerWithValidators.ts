126eabd040ab450450a3a4b76ddfa8225eb695a0
Joshua Browne <joshuabrownenz@gmail.com>
2024-06-17 16:36:13 +1200

    Pre-flatten validators so they don't need to be flattened before every use.
    - Removed `DefineEndpointFunction`
    - Removed `runValidator`
    - Renamed `ValidatorArrayFlat` to `FlatValidatorArray`

--- src/core/endpoint-builder/handlerWithValidators.ts
+++ src/core/endpoint-builder/handlerWithValidators.ts
@@ -1,39 +1,6 @@
 import { ConfigurableHandler } from "../models/ConfigurableHandler";
 import { WithValid } from "../models/DefineEndpointFunction";
-import { KintRequest } from "../models/KintRequest";
-import {
-  ValidatedData,
-  isValidatorArrayFlat,
-  Validator,
-  ValidatorArray,
-} from "../models/Validator";
-
-/**
- * Runs a validator on the given request and updates the `acc` object with the validated data.
- * @template Validators The type of the validator array.
- * @param acc The object to store the validated data.
- * @param validator The validator function to run.
- * @param request The request object to validate.
- * @returns The updated `acc` object with the validated data.
- * @throws Throws an error if the validation fails.
- */
-function runValidator<Validators extends ValidatorArray>(
-  acc: ValidatedData<Validators>,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  validator: Validator<string, any>,
-  request: KintRequest,
-) {
-  const result = validator.validate(request);
-
-  if (result.isValid) {
-    // eslint-disable-next-line no-type-assertion/no-type-assertion -- We know that result.field is a key of acc
-    acc[result.field as keyof typeof acc] = result.parsedData;
-    return acc;
-  } else {
-    throw new Error(result.error);
-  }
-}
-
+import { ValidatedData, FlatValidatorArray } from "../models/Validator";
 /**
  * Takes a handler and wraps it in a new handler which validates the request data.
  * @param innerHandler The handler to wrap.
@@ -43,7 +10,7 @@ function runValidator<Validators extends ValidatorArray>(
 export function wrapHandlerWithValidationLayer<
   Context,
   Config,
-  Validators extends ValidatorArray,
+  Validators extends FlatValidatorArray,
 >(
   innerHandler: ConfigurableHandler<WithValid<Context, Validators>, Config>,
   validators: Validators,
@@ -54,16 +21,15 @@ export function wrapHandlerWithValidationLayer<
     config,
   ) => {
     const validatedData = validators.reduce((acc, validator) => {
-      if (isValidatorArrayFlat(validator)) {
-        validator.reduce((acc, validator) => {
-          runValidator(acc, validator, request);
-          return acc;
-        }, acc);
-        return acc;
-      }
+      const result = validator.validate(request);
 
-      runValidator(acc, validator, request);
-      return acc;
+      if (result.isValid) {
+        // eslint-disable-next-line no-type-assertion/no-type-assertion -- We know that result.field is a key of acc
+        acc[result.field as keyof typeof acc] = result.parsedData;
+        return acc;
+      } else {
+        throw new Error(result.error);
+      }
     }, {} as ValidatedData<Validators>);
 
     (context as WithValid<Context, Validators>).valid = validatedData;
