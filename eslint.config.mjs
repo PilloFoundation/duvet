d4eabe8db4015da6ec3811f4d009cfe616062b1d
Joshua Browne <joshuabrownenz@gmail.com>
2024-06-19 16:05:35 +1200

    Add kintLint to `eslint.config.mjd`

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,10 +4,12 @@ import eslint from "@eslint/js";
 import jsdoc from "eslint-plugin-jsdoc";
 import tseslint from "typescript-eslint";
 import noTypeAssertions from "eslint-plugin-no-type-assertion";
+import kintLint from "@kint/eslint-plugin-kint";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
+  ...kintLint.configs.recommended,
   jsdoc.configs["flat/recommended-typescript"],
   {
     plugins: { jsdoc, "no-type-assertion": noTypeAssertions },
